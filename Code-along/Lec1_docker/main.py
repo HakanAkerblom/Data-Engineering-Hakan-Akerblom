0620557f68d3ac6089730b643bc774d2a1485ee6
Hakan Akerblom <hakan.akerblom@gmail.com>
2023-05-24 21:53:52 +0200

    dash app

--- Code-along/Lec1_docker/main.py
+++ Code-along/Lec1_docker/main.py
@@ -1,5 +1,32 @@
-import sys
+import plotly_express as px
+import numpy as np
+from dash import Dash, dcc, html, Output, Input
 
-print('Hello from the docker side')
+dropdown_options = [{"label": f"{rolls} rolls", "value": rolls} for rolls in [10, 100, 1000, 10000]]
 
-print(f"Python version: {sys.version}")
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H1("Dice simulator"),
+    html.P("Choose number of dice and number of rolls and enjoy the results"),
+    dcc.Graph(id = "dice-graph"),
+    html.H2("Number of rolls"),
+    dcc.Dropdown(id = "number-rolls", options=dropdown_options, value=10),
+    html.H2("Number of dices"),
+    dcc.Slider(id = "number-dices", min=1, max=6, step=1, value=2, )
+
+])
+
+@app.callback(Output("dice-graph", "figure"), Input("number-rolls", "value"), Input("number-dices", "value"))
+
+def _dice_simulator_histogram(rolls, number_dices):
+    dices = np.random.randint(1, 7, size=(rolls, number_dices))
+    return px.histogram(dices.sum(axis=1))
+
+
+
+if __name__ == "__main__":
+    print("Hello from the docker side")
+
+    app.run_server(debug = True)
+
